de6fb2be3190a9c8c7e5f6b85f435ffa19698dc6
shaileshmishra <Yes@383840>
2021-06-17 18:19:53 +0530

    :sparkles: supercharged rte added: json_to_html()

--- contentstack_utils/helper/metadata.py
+++ contentstack_utils/helper/metadata.py
@@ -38,63 +38,28 @@ class Metadata:
 
     @property
     def get_text(self):
-        """text for embedded objects
-
-        Returns:
-            str : text for embedded objects
-        """
         return self.text
 
     @property
     def get_item_type(self):
-        """item_type for embedded objects
-
-        Returns:
-            str :  item_type for embedded objects
-        """
         return self.item_type
 
     @property
     def get_item_uid(self):
-        """item_uid for embedded objects
-
-        Returns:
-            str : item_uid for embedded objects
-        """
         return self.item_uid
 
     @property
     def get_content_type_uid(self):
-        """content_type_uid for embedded objects
-
-        Returns:
-            str :  content_type_uid for embedded objects
-        """
         return self.content_type_uid
 
     @property
     def get_style_type(self) -> StyleType:
-        """style_type for embedded objects
-
-        Returns:
-            StyleType :  style_type for embedded objects
-        """
         return self.style_type
 
     @property
     def get_outer_html(self):
-        """outer_html for embedded objects
-
-        Returns:
-            str :  outer_html for embedded objects
-        """
         return self.outer_html
 
     @property
     def get_attributes(self):
-        """attributes for embedded objects
-
-        Returns:
-            str : attributes for embedded objects
-        """
         return self.attributes
